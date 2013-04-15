091c3e246414fde5664adea217b75da764194938
jawobe <jawobe@jawobe-1215N.(none)>
2013-04-15 19:45:16 +0200

    Stage

--- src/wdec/decision_utils/Stage.java
+++ src/wdec/decision_utils/Stage.java
@@ -1,5 +1,25 @@
 package wdec.decision_utils;
 
 public class Stage {
-
+	
+	private DataIn stageDataIn;
+	private DataOut stageDataTab[];
+	private DataOut stageDataOut;
+	
+	private void findDataOut(DataIn dataIn){
+		
+	}
+	
+	public DataOut findBestData(int risk){
+		
+		return null;
+	}
+	
+	
+	public void Stage(DataIn dataIn){
+		stageDataIn=dataIn;
+		stageDataTab = new DataOut[100];
+		findDataOut(stageDataIn);
+		
+	}
 }
